fd161813f77ff8fa7fff2bfc7701bee261568b30
Jac22 <jacopo.prudentino@gmail.com>
2023-04-14 15:51:56 +0200

    Creata classe LineException

---
+++ LineException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class LineException : Exception
+    {
+        public int Line { get; set; }
+
+        public LineException(int line, string message) : base(message)
+        {
+            Line = line;
+        }
+    }
+}
