00507e9d79f883b024cf2b14c32fb9b821f597cc
Jac22 <jacopo.prudentino@gmail.com>
2023-04-14 15:19:16 +0200

    Completato scheletro base dell'esercizio

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
+            List<Address> addressList = new List<Address>();
             StreamReader sr = File.OpenText("addresses.csv");
+            sr.ReadLine();
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(",");
+                addressList.Add(new Address(line[0], line[1], line[2], line[3], line[4], line[5]));
+                addressList[i].Print();
+                i++;
+            }
+            sr.Close();
         }
     }
 }
