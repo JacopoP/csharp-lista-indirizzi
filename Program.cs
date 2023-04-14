b6d4b7920c443ce0e50c960f01849d488aaf8f55
Jac22 <jacopo.prudentino@gmail.com>
2023-04-14 15:32:43 +0200

    Trattati i casi particolari con try-catch

--- Program.cs
+++ Program.cs
@@ -7,15 +7,39 @@
             List<Address> addressList = new List<Address>();
             StreamReader sr = File.OpenText("addresses.csv");
             sr.ReadLine();
-            int i = 0;
+            int counter = 0;
             while (!sr.EndOfStream)
             {
                 string[] line = sr.ReadLine().Split(",");
-                addressList.Add(new Address(line[0], line[1], line[2], line[3], line[4], line[5]));
-                addressList[i].Print();
-                i++;
+                counter++;
+                try
+                {
+                    if (line.Length > 6)
+                    {
+                        throw new Exception("sono stati inseriti troppi campi!");
+                    }
+                    else if (line.Length < 6)
+                    {
+                        throw new Exception("sono stati inseriti troppi pochi campi!");
+                    }
+                    else
+                    {
+                        foreach (string s in line)
+                            if (s == String.Empty)
+                                throw new Exception("ci sono dei campi vuoti!");
+                        addressList.Add(new Address(line[0], line[1], line[2], line[3], line[4], line[5]));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"A riga {counter} {ex.Message}");
+                }
             }
             sr.Close();
+            foreach (Address address in addressList)
+            {
+                address.Print();
+            }
         }
     }
 }
